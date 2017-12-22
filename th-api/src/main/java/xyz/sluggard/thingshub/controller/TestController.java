923f9780b22daa1c8f54593c64828d14d9e12fb9
frank <sydra@163.com>
2017-12-22 12:42:33 +0800

    初始版本完成

--- th-api/src/main/java/xyz/sluggard/thingshub/controller/TestController.java
+++ th-api/src/main/java/xyz/sluggard/thingshub/controller/TestController.java
@@ -1,6 +1,11 @@
 package xyz.sluggard.thingshub.controller;
 
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +18,32 @@ public class TestController {
 	public String test() {
 		log.info("success");
 		return "success";
+	}
+	
+	@RequestMapping(value="test", headers="api_version=2.0")
+	public String test2(HttpServletRequest req) {
+		return "api2.0";
+	}
+	
+	@RequestMapping(value="test")
+	public String test1(HttpServletRequest req) {
+		return "api1.0";
+	}
+	
+	@RequestMapping(value="test", headers= {"api_version=2.0","client_version=ios"})
+	public String test3(HttpServletRequest req) {
+		return "api3.0";
+	}
+	
+	
+	@RequestMapping(value="testClient", headers= {"client_version=ios"})
+	public String testchannel1() {
+		return "clientios";
+	}
+	
+	@RequestMapping(value="testClient", headers= {})
+	public String testchannel2() {
+		return "clientandroid";
 	}
 
 }
