be09ab48cb9aa68b099b9f26a6194343c3e51da1
frank <sydra@163.com>
2017-12-25 15:53:42 +0800

    update

--- th-api/src/main/java/xyz/sluggard/thingshub/controller/TestController.java
+++ th-api/src/main/java/xyz/sluggard/thingshub/controller/TestController.java
@@ -1,13 +1,11 @@
 package xyz.sluggard.thingshub.controller;
 
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.Enumeration;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
